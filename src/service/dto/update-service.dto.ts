921ac0e8afaa0c60705dede2eb1af70df30383a3
zaighumhassan6 <zaighumrajput82@gmail.com>
2024-07-07 14:35:32 +0500

    service module Updated

--- src/service/dto/update-service.dto.ts
+++ src/service/dto/update-service.dto.ts
@@ -2,29 +2,23 @@ import { IsString, IsNumber, IsOptional, IsNotEmpty } from 'class-validator';
 
 export class UpdateServiceDto {
   @IsNotEmpty()
-  @IsNumber()
   shopId: number;
 
-  @IsNumber()
   @IsNotEmpty()
   categoryId: number;
 
   @IsNotEmpty()
-  @IsNumber()
   id: number; //Service Id
   @IsString()
   @IsOptional()
   name?: string;
 
-  @IsNumber()
   @IsOptional()
   price?: number;
 
-  @IsNumber()
   @IsOptional()
   duration?: number;
 
-  @IsNumber()
   @IsOptional()
   maxService?: number;
 
--- src/service/service.controller.ts
+++ src/service/service.controller.ts
@@ -24,7 +24,7 @@ export class ServiceController {
   constructor(private readonly serviceService: ServiceService) {}
 
   @Post('create')
-  @UseGuards(SuperAdminGuards)
+  // @UseGuards(SuperAdminGuards)
   create(@Body() dto: CreateServiceDto) {
     return this.serviceService.create(dto);
   }
@@ -34,9 +34,39 @@ export class ServiceController {
     return this.serviceService.findAll(body.id);
   }
 
+  // @Post('update')
+  // updateShop(@Body() dto: UpdateServiceDto) {
+  //   return this.serviceService.updateService(dto);
+  // }
   @Post('update')
-  updateShop(@Body() dto: UpdateServiceDto) {
-    return this.serviceService.updateService(dto);
+  @UseInterceptors(
+    FilesInterceptor('picture', 10, {
+      storage: diskStorage({
+        destination: './public/uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${file.fieldname}-${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  async updateService(
+    @UploadedFiles() files: Express.Multer.File[], // Use UploadedFiles to handle multiple files
+    @Body() dto: UpdateServiceDto,
+  ) {
+    dto.shopId = Number(dto.shopId);
+    dto.categoryId = Number(dto.categoryId);
+    dto.price = Number(dto.price);
+    dto.duration = Number(dto.duration);
+    dto.maxService = Number(dto.maxService);
+    dto.id = Number(dto.id);
+    const pictureUrls = files.map((file) => file.path.replace(/\\/g, '/')); // Replace backslashes with forward slashes for compatibility
+    const serviceData = { ...dto, picture: pictureUrls };
+
+    return this.serviceService.updateService(serviceData);
   }
 
   @Delete('id')
@@ -44,7 +74,7 @@ export class ServiceController {
     return this.serviceService.delete(body.id);
   }
 
-  @Post('upload')
+  @Post('create-service')
   @UseInterceptors(
     FilesInterceptor('picture', 10, {
       storage: diskStorage({
